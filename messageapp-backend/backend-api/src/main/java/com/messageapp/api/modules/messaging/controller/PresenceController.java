24dc125a749b99d7f0dee8dd89464f47d546ab86
Manovate <manovate15@gmail.com>
2026-01-05 12:18:25 +0530

    Disable PresenceController since PresenceService is not implemented
    Comment out the PresenceController class and its methods.

--- messageapp-backend/backend-api/src/main/java/com/messageapp/api/modules/messaging/controller/PresenceController.java
+++ messageapp-backend/backend-api/src/main/java/com/messageapp/api/modules/messaging/controller/PresenceController.java
@@ -6,31 +6,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-//@Controller
-//public class PresenceController {
-//
-//    private final PresenceService presenceService;
-//
-//    public PresenceController(PresenceService presenceService) {
-//        this.presenceService = presenceService;
-//    }
-//
-//    // user comes online
-//    @MessageMapping("/presence/online")
-//    @SendTo("/topic/presence")
-//    public UserPresenceDTO online(String userId) {
-//        return presenceService.userOnline(userId);
-//    }
-//
-//    // user goes offline
-//    @MessageMapping("/presence/offline")
-//    @SendTo("/topic/presence")
-//    public UserPresenceDTO offline(String userId) {
-//        return presenceService.userOffline(userId);
-//    }
-//}
 
-@Controller
+//@Controller
 public class PresenceController {
 
     private final PresenceService presenceService;
