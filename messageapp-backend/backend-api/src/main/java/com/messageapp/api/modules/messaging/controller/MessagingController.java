7da0b7d96e94905f38121067428f96934d2de2a6
Dhiviyan <dhiviyan123@gmail.com>
2026-01-20 18:21:35 +0530

    Refactor messaging controller and update pom configuration

--- messageapp-backend/backend-api/src/main/java/com/messageapp/api/modules/messaging/controller/MessagingController.java
+++ messageapp-backend/backend-api/src/main/java/com/messageapp/api/modules/messaging/controller/MessagingController.java
@@ -7,16 +7,15 @@ import com.messageapp.api.modules.messaging.service.MessagingService;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/messages")
-public class MessageController {
+public class MessagingController {
 
 	private final MessagingService messagingService;
 
-	public MessageController(MessagingService messagingService) {
+	public MessagingController(MessagingService messagingService) {
 		this.messagingService = messagingService;
 	}
 
@@ -34,11 +33,12 @@ public class MessageController {
 //	}
 
 	// 2 pagition
-		@GetMapping("/{conversationId}")
-		public Page<Message> getMessages(@PathVariable UUID conversationId, @RequestParam(defaultValue = "0") int page,
-				@RequestParam(defaultValue = "20") int size) {
-			return messagingService.getMessagesByConversation(conversationId, page, size);
-		}
+	@GetMapping("/{conversationId}")
+	public Page<Message> getMessages(@PathVariable UUID conversationId, @RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "20") int size) {
+		return messagingService.getMessagesByConversation(conversationId, page, size);
+	}
+
 	// 3️⃣ MARK DELIVERED
 	@PutMapping("/{conversationId}/delivered")
 	public void markDelivered(@PathVariable UUID conversationId) {
@@ -50,17 +50,12 @@ public class MessageController {
 	public void markRead(@PathVariable UUID conversationId, @PathVariable UUID userId) {
 		messagingService.markMessagesAsRead(conversationId, userId);
 	}
-	
+
 	// 5 unread
-	
+
 	@GetMapping("/{conversationId}/unread/{userId}")
-	public long getUnreadCount(
-	        @PathVariable UUID conversationId,
-	        @PathVariable UUID userId
-	) {
-	    return messagingService.getUnreadCount(conversationId, userId);
+	public long getUnreadCount(@PathVariable UUID conversationId, @PathVariable UUID userId) {
+		return messagingService.getUnreadCount(conversationId, userId);
 	}
 
-
-	
 }
