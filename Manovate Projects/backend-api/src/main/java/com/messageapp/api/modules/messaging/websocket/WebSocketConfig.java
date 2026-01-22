01bcac8d44994cd9dbad177cdf8ff4a3562b8020
Manovate <manovate15@gmail.com>
2026-01-22 16:46:50 +0530

    Update WebSocketConfig.java

--- Manovate Projects/backend-api/src/main/java/com/messageapp/api/modules/messaging/websocket/WebSocketConfig.java
+++ Manovate Projects/backend-api/src/main/java/com/messageapp/api/modules/messaging/websocket/WebSocketConfig.java
@@ -2,27 +2,20 @@ package com.messageapp.api.modules.messaging.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // messages sent to /topic go to subscribers
-        registry.enableSimpleBroker("/topic");
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
+	}
 
-        // messages sent from client start with /app
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
+	}
 }
