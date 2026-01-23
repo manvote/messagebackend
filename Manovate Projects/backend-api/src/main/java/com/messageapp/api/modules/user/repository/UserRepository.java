367e77b61aad398dd9604e4f16e3cafe7c0d5568
Manovate <manovate15@gmail.com>
2026-01-23 17:32:24 +0530

    Update UserRepository.java

--- Manovate Projects/backend-api/src/main/java/com/messageapp/api/modules/user/repository/UserRepository.java
+++ Manovate Projects/backend-api/src/main/java/com/messageapp/api/modules/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.messageapp.api.modules.user.repository;
 import com.messageapp.api.modules.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 🔹 FIND USER BY PHONE NUMBER
+    Optional<User> findByPhone(String phone);
 }
